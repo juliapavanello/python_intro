97d020ee1a12e4b4a67709c31c6e8ab356276d5b
juliapavanello <julia.pavanello21@gmail.com>
2025-11-17 21:39:40 -0300

    Update teste.py

--- labs/teste.py
+++ labs/teste.py
@@ -1,1 +1,15 @@
 import networkx as nx
+
+with open('downloads\le450_5a.col.txt', 'r', encoding='utf-8') as f:
+    for linha in f:
+        if(linha.__contains__('number of vertices')):
+            num = linha[24:27]
+            print(num)
+        
+c = []
+for i in num:
+    c.append(i)
+
+print(c)
+
+        
